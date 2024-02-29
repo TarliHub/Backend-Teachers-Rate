ae4f06b0baf9f07337f69a73f7eab9f709d90caa
DmytroProg <dmytroprog@gmail.com>
2024-03-01 00:11:11 +0200

    added user model

---
+++ TeacherRateProject/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeacherRateProject.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(64)]
+    public required string Name { get; set; }
+
+    [Required]
+    [MaxLength(64)]
+    public required string Login { get; set; }
+
+    [Required]
+    [MaxLength(64)]
+    public required string Password { get; set; }
+}
