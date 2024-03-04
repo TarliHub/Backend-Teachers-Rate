74528a66e849710152eb4938b6518dfe46f3a2d1
DmytroProg <dmytroprog@gmail.com>
2024-03-05 01:16:48 +0200

    Added helpers

---
+++ TeacherRateProject/Helpers/CompositionRoot.cs
@@ -0,0 +1,16 @@
+﻿using TeacherRateProject.DTOs;
+using TeacherRateProject.Services.Interfaces;
+using TeacherRateProject.Services;
+using TeacherRateProject.Data.Repository.Interfaces;
+using TeacherRateProject.Data.Repository.SqlRepository;
+
+namespace TeacherRateProject.Helpers;
+
+public static class CompositionRoot
+{
+    public static void AddUserServices(this IServiceCollection services)
+    {
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<IUserService<UserDto, int>, UserService>();
+    }
+}
---
+++ TeacherRateProject/Helpers/StringHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace TeacherRateProject.Helpers;
+
+public static class StringHelper
+{
+    public static bool IsEmptyString(this string value)
+    {
+        return value.Trim().IsNullOrEmpty();
+    }
+}
