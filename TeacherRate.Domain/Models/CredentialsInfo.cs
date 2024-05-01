c08f6b2b772e66b2e11c6f30fffd84fc7100a694
DmytroProg <dmytroprog@gmail.com>
2024-05-01 14:21:15 +0300

    Added password hashing logic

---
+++ TeacherRate.Domain/Models/CredentialsInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeacherRate.Domain.Models;
+
+public class CredentialsInfo
+{
+    public int Id { get; set; }
+    public required string PasswordHash { get; set; }
+
+    [ForeignKey("FK_Creds_User")]
+    public int UserId { get; set; }
+}
---
+++ TeacherRate.Helpers/PasswordManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TeacherRate.Helpers;
+
+public static class PasswordManager
+{
+    public static string ComputeHashPassword(string password)
+    {
+        var sha = SHA256.Create();
+
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var shaPassword = sha.ComputeHash(bytes);
+
+        return Convert.ToBase64String(shaPassword);
+    }
+}
