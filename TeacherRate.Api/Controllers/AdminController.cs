606e2e011bfa66f8d5c5535cb0c1ab6d824d56da
DmytroProg <dmytroprog@gmail.com>
2024-05-01 13:28:27 +0300

    added admin controller

---
+++ TeacherRate.Api/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeacherRate.Api.Controllers;
+
+[Route("api/admin")]
+[ApiController]
+public class AdminController : ControllerBase
+{
+    public AdminController()
+    {
+        
+    }
+}
