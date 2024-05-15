51e7fe525d4c11abd0cb658cbbe3fc63ef501503
DmytroProg <dmytroprog@gmail.com>
2024-05-15 16:06:30 +0300

    added basic endpoints for tasks

--- TeacherRate.Api/Controllers/TaskController.cs
+++ TeacherRate.Api/Controllers/TaskController.cs
@@ -1,6 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using TeacherRate.Api.DTOs;
+using TeacherRate.Api.Models.Paging;
+using TeacherRate.Api.Models.Requests;
+using TeacherRate.Api.Services;
 using TeacherRate.Domain.Interfaces;
+using TeacherRate.Domain.Models;
 namespace TeacherRate.Api.Controllers;
 
 [Route("api/tasks")]
@@ -15,5 +20,47 @@ public class TaskController : ControllerBase
         _taskService = taskService;
         _mapper = mapper;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedList<UserTaskDTO>>> GetUserTasks(
+        [FromQuery] PageRequest pageRequest)
+    {
+        var tasks = _taskService.GetTasks();
+        var page = tasks.ToPagedList(pageRequest.Page, pageRequest.Size);
+
+        return Ok(page.Map<UserTaskDTO>(_mapper));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedList<CompletedTaskDTO>>> GetCompletedUserTasks(
+        [FromQuery] PageRequest pageRequest)
+    {
+        var id = HttpContext.Session.GetInt32("UserId");
+        if (!id.HasValue)
+            return Unauthorized();
+
+        var tasks = await _taskService.GetUserTasks<TeacherBase>(id.Value);
+
+        var page = tasks.ToPagedList(pageRequest.Page, pageRequest.Size);
+
+        return Ok(page.Map<UserTaskDTO>(_mapper));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserTaskDTO>> GetUserTaskById(int id)
+    {
+        var task = await _taskService.GetTaskById(id);
+
+        return Ok(_mapper.Map<UserTaskDTO>(task));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserTaskDTO>> AddUserTask(UserTaskDTO userTaskDTO)
+    {
+        var task = await _taskService.AddTask(_mapper.Map<UserTask>(userTaskDTO));
+
+        return Ok(_mapper.Map<UserTaskDTO>(task));
+    }
+
 
 }
--- TeacherRate.Domain/Interfaces/ITaskService.cs
+++ TeacherRate.Domain/Interfaces/ITaskService.cs
@@ -5,9 +5,8 @@ namespace TeacherRate.Domain.Interfaces;
 public interface ITaskService
 {
     Task<bool> SendTask(TeacherRequest request);
-    Task<List<CompletedTask>> GetCompletedTasks(int index, int size);
     IQueryable<UserTask> GetTasks();
-    Task<List<CompletedTask>> GetUserTasks<T>(int id) where T : TeacherBase;
+    Task<IQueryable<CompletedTask>> GetUserTasks<T>(int id) where T : TeacherBase;
     Task<UserTask?> GetTaskById(int id);
     Task<UserTask> AddTask(UserTask task);
     Task RemoveTask(int id);
